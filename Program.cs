91a5a6277e2eb3e0d849832936e4935a42c254ad
Judisk <122523925+Judisk@users.noreply.github.com>
2023-03-14 07:10:25 +0300

    redact AppEnd Method and final

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
 ﻿string [] Result = {""};
 
+ while (true)
+ {
+    Console.WriteLine("Введите 0000 если хотите закончить ввод");
+     string Word = Console.ReadLine();
+     if (Word != "0000") LongWordThree(Word, ref Result);
+     else {ShowArray(Result); return;}
+
+}
+
+// LongWordThree("sad",ref Result);
+// ShowArray(Result);
+// LongWordThree("sa",ref Result);
+
+// ShowArray(Result);
+
 void LongWordThree(string word,ref string[] Result )
 {
     int Long = word.Length;
@@ -9,9 +24,14 @@ void LongWordThree(string word,ref string[] Result )
 void AppEnd(ref string[] Result,string word)
 {
     string[] Res = new string[Result.Length+1];
-    Res[Res.Length - 1] = word;
+    for (int i = 0; i < Result.Length; i++)
+    {
+        Res[i] = Result[i];
+    }
+    Res[Res.Length-1] = word;
     Result = Res;
 }
+
 
 void ShowArray(string[] testArray)
 {
@@ -19,3 +39,4 @@ void ShowArray(string[] testArray)
     var str = string.Join(" ", showArray);
     Console.WriteLine(str);
 }
+
