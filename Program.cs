1450bdf51ee8c4cebd73356a45c1aaf812af772d
Judisk <122523925+Judisk@users.noreply.github.com>
2023-03-14 06:59:25 +0300

    create AppEnd Method

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string [] Result = {""};
+
+void AppEnd(ref string[] Result,string word)
+{
+    string[] Res = new string[Result.Length+1];
+    Res[Res.Length - 1] = word;
+    Result = Res;
+}
+
